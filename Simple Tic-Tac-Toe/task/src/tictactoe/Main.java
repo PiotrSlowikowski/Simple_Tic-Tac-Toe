cadc4686a2b0266f9ffd9b5722f146dc862fddc8
PiotrSlowikowski <peter.slowikowski@hotmail.com>
2021-06-03 13:34:09 +0200

    Check win conditions, correctness of figures on the grid and game state implemented

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -8,28 +8,146 @@ public class Main {
         Grid grid = new Grid();
         char[][] createdGrid = grid.createGrid();
         grid.printGrid(createdGrid);
-        grid.checkScore(createdGrid);
+        grid.checkGameState(createdGrid);
+
     }
 }
 
 class Grid {
 
-    public static void checkScore(char[][] grid) {
+    public static void checkGameState(char[][] grid) {
 
-        System.out.println("LOL");
+
+        if (checkFiguresCountCorrectness(grid) && checkIfOnlyOneWinPatternExists(grid)) {
+            if (!checkWinConditions(grid)) {
+                checkDraw(grid);
+                checkIfGameIsFinished(grid);
+            }
+        }
 
     }
 
-    public static boolean isGameFinished(char[][] grid) {
-        boolean isFinished = true;
-        for (int i=0; i<grid.length; i++) {
-            for (int j=0; j<grid[i].length; j++) {
-                if (grid[i][j] == '_') {
-                    isFinished = false;
+    public static boolean checkIfOnlyOneWinPatternExists(char[][] grid) {
+
+        boolean isOnlyOneWinPatternPresent = true;
+        int patternCounter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
+                patternCounter++;
+            } else if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
+                patternCounter++;
+            }
+        }
+        if (checkNumberOfDiagonalPatterns(grid) > 0) {
+            patternCounter += checkNumberOfDiagonalPatterns(grid);
+        }
+
+        if (patternCounter > 1) {
+            isOnlyOneWinPatternPresent = false;
+            System.out.println("Impossible");
+        }
+        return isOnlyOneWinPatternPresent;
+    }
+
+
+    public static int checkNumberOfDiagonalPatterns(char[][] grid) {
+        int counter = 0;
+        if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) {
+            counter++;
+        }
+        if (grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2]) {
+            counter++;
+        }
+        return counter;
+    }
+
+
+    public static boolean checkFiguresCountCorrectness(char[][] grid) {
+
+        boolean isFigureCountCorrect = true;
+        int Xcounter = 0;
+        int Ocounter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 'X') {
+                    Xcounter++;
+                } else if (grid[i][j] == 'O') {
+                    Ocounter++;
                 }
             }
         }
-        return isFinished;
+
+        if (Xcounter - Ocounter >= 2 || Ocounter - Xcounter >= 2) {
+            System.out.println("Impossible");
+            isFigureCountCorrect = false;
+        }
+        return isFigureCountCorrect;
+    }
+
+    public static boolean checkIfGameIsFinished(char[][] grid) {
+
+        boolean isGameFinished = false;
+        if (!checkWinConditions(grid) && !checkForEmptyCells(grid)) {
+            isGameFinished = true;
+        } else {
+            System.out.println("Game not finished");
+        }
+        return isGameFinished;
+    }
+
+
+    public static boolean checkDraw(char[][] grid) {
+
+        boolean isGameDraw = false;
+
+        if (!checkWinConditions(grid) && !checkForEmptyCells(grid)) {
+            System.out.println("Draw");
+            isGameDraw = true;
+        }
+
+        return isGameDraw;
+    }
+
+    public static boolean checkWinConditions(char[][] grid) {
+
+        boolean isWinConditionPresent = false;
+
+        for (int i = 0; i < grid.length; i++) {
+            if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2]) {
+                System.out.println(grid[i][0] + " wins");
+                isWinConditionPresent = !isWinConditionPresent;
+                break;
+            } else if (grid[0][i] == grid[1][i] && grid[1][i] == grid[2][i]) {
+                System.out.println(grid[0][i] + " wins");
+                isWinConditionPresent = !isWinConditionPresent;
+                break;
+            } else if (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) {
+                System.out.println(grid[0][0] + " wins");
+                isWinConditionPresent = !isWinConditionPresent;
+                break;
+            } else if (grid[2][0] == grid[1][1] && grid[1][1] == grid[0][2]) {
+                System.out.println(grid[2][0] + " wins");
+                isWinConditionPresent = !isWinConditionPresent;
+                break;
+            }
+        }
+        return isWinConditionPresent;
+    }
+
+
+    public static boolean checkForEmptyCells(char[][] grid) {
+
+        boolean isEmptyCellPresent = false;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '_') {
+                    isEmptyCellPresent = true;
+                }
+            }
+        }
+        return isEmptyCellPresent;
     }
 
     public static void printGrid(char[][] field) {
